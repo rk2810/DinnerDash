64751f377b92824dfc96a306d740047247f1aad1
rk2810 <rohan.kumquat@gmail.com>
2018-07-24 23:38:23 +0530

    Button: Add Dash button functionality. - Picks a random item from list. - Some UI changes as usual.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- app/src/main/java/com/jade/rohank/dinnerdash/MainActivity.kt
+++ app/src/main/java/com/jade/rohank/dinnerdash/MainActivity.kt
@@ -2,11 +2,22 @@ package com.jade.rohank.dinnerdash
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var foodList = arrayOf("Om's Dosa", "Rao's Chaap", "Paneer Lababdaar", "Pizza Hut", "Burger King", "McDonald's")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dashButton.setOnClickListener {
+            val rand = Random()
+            val randFood = rand.nextInt(foodList.size)
+            decidedFood.setText(foodList[randFood])
+        }
+
     }
 }
